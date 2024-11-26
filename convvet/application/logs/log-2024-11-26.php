e5718ebbf704e120c8034e611754ce11b2d98ca3
kinko2006 <rafael.alfalci@gmail.com>
2024-11-26 13:38:39 -0300

    a

---
+++ convvet/application/logs/log-2024-11-26.php
@@ -0,0 +1,53 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2024-11-26 16:37:50 --> Config Class Initialized
+INFO - 2024-11-26 16:37:50 --> Hooks Class Initialized
+DEBUG - 2024-11-26 16:37:50 --> UTF-8 Support Enabled
+INFO - 2024-11-26 16:37:50 --> Utf8 Class Initialized
+INFO - 2024-11-26 16:37:50 --> URI Class Initialized
+DEBUG - 2024-11-26 16:37:50 --> No URI present. Default controller set.
+INFO - 2024-11-26 16:37:50 --> Router Class Initialized
+INFO - 2024-11-26 16:37:50 --> Output Class Initialized
+INFO - 2024-11-26 16:37:50 --> Security Class Initialized
+DEBUG - 2024-11-26 16:37:50 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 16:37:50 --> Input Class Initialized
+INFO - 2024-11-26 16:37:50 --> Language Class Initialized
+INFO - 2024-11-26 16:37:50 --> Loader Class Initialized
+INFO - 2024-11-26 16:37:50 --> Database Driver Class Initialized
+ERROR - 2024-11-26 16:37:50 --> Unable to connect to the database
+INFO - 2024-11-26 16:37:50 --> Language file loaded: language/english/db_lang.php
+INFO - 2024-11-26 16:38:17 --> Config Class Initialized
+INFO - 2024-11-26 16:38:17 --> Hooks Class Initialized
+DEBUG - 2024-11-26 16:38:17 --> UTF-8 Support Enabled
+INFO - 2024-11-26 16:38:17 --> Utf8 Class Initialized
+INFO - 2024-11-26 16:38:17 --> URI Class Initialized
+DEBUG - 2024-11-26 16:38:17 --> No URI present. Default controller set.
+INFO - 2024-11-26 16:38:17 --> Router Class Initialized
+INFO - 2024-11-26 16:38:17 --> Output Class Initialized
+INFO - 2024-11-26 16:38:17 --> Security Class Initialized
+DEBUG - 2024-11-26 16:38:17 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 16:38:17 --> Input Class Initialized
+INFO - 2024-11-26 16:38:17 --> Language Class Initialized
+INFO - 2024-11-26 16:38:17 --> Loader Class Initialized
+INFO - 2024-11-26 16:38:17 --> Database Driver Class Initialized
+ERROR - 2024-11-26 16:38:17 --> Unable to connect to the database
+INFO - 2024-11-26 16:38:17 --> Language file loaded: language/english/db_lang.php
+INFO - 2024-11-26 16:38:30 --> Config Class Initialized
+INFO - 2024-11-26 16:38:30 --> Hooks Class Initialized
+DEBUG - 2024-11-26 16:38:30 --> UTF-8 Support Enabled
+INFO - 2024-11-26 16:38:30 --> Utf8 Class Initialized
+INFO - 2024-11-26 16:38:30 --> URI Class Initialized
+DEBUG - 2024-11-26 16:38:30 --> No URI present. Default controller set.
+INFO - 2024-11-26 16:38:30 --> Router Class Initialized
+INFO - 2024-11-26 16:38:30 --> Output Class Initialized
+INFO - 2024-11-26 16:38:30 --> Security Class Initialized
+DEBUG - 2024-11-26 16:38:30 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 16:38:30 --> Input Class Initialized
+INFO - 2024-11-26 16:38:30 --> Language Class Initialized
+INFO - 2024-11-26 16:38:30 --> Loader Class Initialized
+INFO - 2024-11-26 16:38:30 --> Database Driver Class Initialized
+INFO - 2024-11-26 16:38:30 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 16:38:30 --> Controller Class Initialized
+INFO - 2024-11-26 16:38:30 --> File loaded: /Users/larger/Desktop/github/malado/convvet/convvet/application/views/home_view.php
+INFO - 2024-11-26 16:38:30 --> Final output sent to browser
+DEBUG - 2024-11-26 16:38:30 --> Total execution time: 0.0650
