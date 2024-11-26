40764679053d1c263196eced49c21647f0a6a1ba
kinko2006 <rafael.alfalci@gmail.com>
2024-11-26 20:00:26 -0300

    a

--- convvet/application/logs/log-2024-11-26.php
+++ convvet/application/logs/log-2024-11-26.php
@@ -51,3 +51,219 @@ INFO - 2024-11-26 16:38:30 --> Controller Class Initialized
 INFO - 2024-11-26 16:38:30 --> File loaded: /Users/larger/Desktop/github/malado/convvet/convvet/application/views/home_view.php
 INFO - 2024-11-26 16:38:30 --> Final output sent to browser
 DEBUG - 2024-11-26 16:38:30 --> Total execution time: 0.0650
+INFO - 2024-11-26 22:11:11 --> Config Class Initialized
+INFO - 2024-11-26 22:11:11 --> Hooks Class Initialized
+DEBUG - 2024-11-26 22:11:11 --> UTF-8 Support Enabled
+INFO - 2024-11-26 22:11:11 --> Utf8 Class Initialized
+INFO - 2024-11-26 22:11:11 --> URI Class Initialized
+DEBUG - 2024-11-26 22:11:11 --> No URI present. Default controller set.
+INFO - 2024-11-26 22:11:11 --> Router Class Initialized
+INFO - 2024-11-26 22:11:11 --> Output Class Initialized
+INFO - 2024-11-26 22:11:11 --> Security Class Initialized
+DEBUG - 2024-11-26 22:11:11 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 22:11:11 --> Input Class Initialized
+INFO - 2024-11-26 22:11:11 --> Language Class Initialized
+INFO - 2024-11-26 22:11:11 --> Loader Class Initialized
+INFO - 2024-11-26 22:11:11 --> Database Driver Class Initialized
+INFO - 2024-11-26 22:11:12 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 22:11:12 --> Controller Class Initialized
+INFO - 2024-11-26 22:11:12 --> File loaded: /Users/larger/Desktop/github/malado/convvet/convvet/application/views/home_view.php
+INFO - 2024-11-26 22:11:12 --> Final output sent to browser
+DEBUG - 2024-11-26 22:11:12 --> Total execution time: 0.4024
+INFO - 2024-11-26 22:58:31 --> Config Class Initialized
+INFO - 2024-11-26 22:58:31 --> Hooks Class Initialized
+DEBUG - 2024-11-26 22:58:31 --> UTF-8 Support Enabled
+INFO - 2024-11-26 22:58:31 --> Utf8 Class Initialized
+INFO - 2024-11-26 22:58:31 --> URI Class Initialized
+DEBUG - 2024-11-26 22:58:31 --> No URI present. Default controller set.
+INFO - 2024-11-26 22:58:31 --> Router Class Initialized
+INFO - 2024-11-26 22:58:31 --> Output Class Initialized
+INFO - 2024-11-26 22:58:31 --> Security Class Initialized
+DEBUG - 2024-11-26 22:58:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 22:58:31 --> Input Class Initialized
+INFO - 2024-11-26 22:58:31 --> Language Class Initialized
+INFO - 2024-11-26 22:58:31 --> Loader Class Initialized
+INFO - 2024-11-26 22:58:31 --> Database Driver Class Initialized
+INFO - 2024-11-26 22:58:31 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 22:58:31 --> Controller Class Initialized
+INFO - 2024-11-26 22:58:31 --> File loaded: /Users/larger/Desktop/github/malado/convvet/convvet/application/views/home_view.php
+INFO - 2024-11-26 22:58:31 --> Final output sent to browser
+DEBUG - 2024-11-26 22:58:31 --> Total execution time: 0.1563
+INFO - 2024-11-26 22:59:31 --> Config Class Initialized
+INFO - 2024-11-26 22:59:31 --> Hooks Class Initialized
+DEBUG - 2024-11-26 22:59:31 --> UTF-8 Support Enabled
+INFO - 2024-11-26 22:59:31 --> Utf8 Class Initialized
+INFO - 2024-11-26 22:59:31 --> URI Class Initialized
+DEBUG - 2024-11-26 22:59:31 --> No URI present. Default controller set.
+INFO - 2024-11-26 22:59:31 --> Router Class Initialized
+INFO - 2024-11-26 22:59:31 --> Output Class Initialized
+INFO - 2024-11-26 22:59:31 --> Security Class Initialized
+DEBUG - 2024-11-26 22:59:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 22:59:31 --> Input Class Initialized
+INFO - 2024-11-26 22:59:31 --> Language Class Initialized
+INFO - 2024-11-26 22:59:31 --> Loader Class Initialized
+INFO - 2024-11-26 22:59:31 --> Database Driver Class Initialized
+INFO - 2024-11-26 22:59:31 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 22:59:31 --> Controller Class Initialized
+INFO - 2024-11-26 22:59:31 --> File loaded: /Users/larger/Desktop/github/malado/convvet/convvet/application/views/home_view.php
+INFO - 2024-11-26 22:59:31 --> Final output sent to browser
+DEBUG - 2024-11-26 22:59:31 --> Total execution time: 0.0395
+INFO - 2024-11-26 22:59:33 --> Config Class Initialized
+INFO - 2024-11-26 22:59:33 --> Hooks Class Initialized
+DEBUG - 2024-11-26 22:59:33 --> UTF-8 Support Enabled
+INFO - 2024-11-26 22:59:33 --> Utf8 Class Initialized
+INFO - 2024-11-26 22:59:33 --> URI Class Initialized
+INFO - 2024-11-26 22:59:33 --> Router Class Initialized
+INFO - 2024-11-26 22:59:33 --> Output Class Initialized
+INFO - 2024-11-26 22:59:33 --> Security Class Initialized
+DEBUG - 2024-11-26 22:59:33 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 22:59:33 --> Input Class Initialized
+INFO - 2024-11-26 22:59:33 --> Language Class Initialized
+INFO - 2024-11-26 22:59:33 --> Loader Class Initialized
+INFO - 2024-11-26 22:59:33 --> Database Driver Class Initialized
+INFO - 2024-11-26 22:59:33 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 22:59:33 --> Controller Class Initialized
+INFO - 2024-11-26 22:59:33 --> Helper loaded: url_helper
+DEBUG - 2024-11-26 22:59:33 --> Session class already loaded. Second attempt ignored.
+INFO - 2024-11-26 22:59:33 --> Model "User_model" initialized
+INFO - 2024-11-26 22:59:33 --> Config Class Initialized
+INFO - 2024-11-26 22:59:33 --> Hooks Class Initialized
+DEBUG - 2024-11-26 22:59:33 --> UTF-8 Support Enabled
+INFO - 2024-11-26 22:59:33 --> Utf8 Class Initialized
+INFO - 2024-11-26 22:59:33 --> URI Class Initialized
+INFO - 2024-11-26 22:59:33 --> Router Class Initialized
+INFO - 2024-11-26 22:59:33 --> Output Class Initialized
+INFO - 2024-11-26 22:59:33 --> Security Class Initialized
+DEBUG - 2024-11-26 22:59:33 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 22:59:33 --> Input Class Initialized
+INFO - 2024-11-26 22:59:33 --> Language Class Initialized
+INFO - 2024-11-26 22:59:33 --> Loader Class Initialized
+INFO - 2024-11-26 22:59:33 --> Database Driver Class Initialized
+INFO - 2024-11-26 22:59:33 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 22:59:33 --> Controller Class Initialized
+INFO - 2024-11-26 22:59:33 --> Helper loaded: url_helper
+DEBUG - 2024-11-26 22:59:33 --> Session class already loaded. Second attempt ignored.
+INFO - 2024-11-26 22:59:33 --> Model "User_model" initialized
+INFO - 2024-11-26 22:59:33 --> File loaded: /Users/larger/Desktop/github/malado/convvet/convvet/application/views/auth/login.php
+INFO - 2024-11-26 22:59:33 --> Final output sent to browser
+DEBUG - 2024-11-26 22:59:33 --> Total execution time: 0.0422
+INFO - 2024-11-26 22:59:38 --> Config Class Initialized
+INFO - 2024-11-26 22:59:38 --> Hooks Class Initialized
+DEBUG - 2024-11-26 22:59:38 --> UTF-8 Support Enabled
+INFO - 2024-11-26 22:59:38 --> Utf8 Class Initialized
+INFO - 2024-11-26 22:59:38 --> URI Class Initialized
+INFO - 2024-11-26 22:59:38 --> Router Class Initialized
+INFO - 2024-11-26 22:59:38 --> Output Class Initialized
+INFO - 2024-11-26 22:59:38 --> Security Class Initialized
+DEBUG - 2024-11-26 22:59:38 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 22:59:38 --> Input Class Initialized
+INFO - 2024-11-26 22:59:38 --> Language Class Initialized
+INFO - 2024-11-26 22:59:38 --> Loader Class Initialized
+INFO - 2024-11-26 22:59:38 --> Database Driver Class Initialized
+INFO - 2024-11-26 22:59:38 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 22:59:38 --> Controller Class Initialized
+INFO - 2024-11-26 22:59:38 --> Helper loaded: url_helper
+DEBUG - 2024-11-26 22:59:38 --> Session class already loaded. Second attempt ignored.
+INFO - 2024-11-26 22:59:38 --> Model "User_model" initialized
+DEBUG - 2024-11-26 22:59:38 --> Email: clinica@gmail.com, Role: clinic
+ERROR - 2024-11-26 22:59:38 --> Severity: Warning --> SQLite3::query(): Unable to prepare statement: 1, no such table: users /Users/larger/Desktop/github/malado/convvet/convvet/system/database/drivers/sqlite3/sqlite3_driver.php 130
+ERROR - 2024-11-26 22:59:38 --> Query error: no such table: users - Invalid query: SELECT *
+FROM "users"
+WHERE "email" = 'clinica@gmail.com'
+AND "role" = 'clinic'
+INFO - 2024-11-26 22:59:38 --> Language file loaded: language/english/db_lang.php
+ERROR - 2024-11-26 22:59:38 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Users/larger/Desktop/github/malado/convvet/convvet/system/core/Exceptions.php:272) /Users/larger/Desktop/github/malado/convvet/convvet/system/core/Common.php 571
+INFO - 2024-11-26 23:00:16 --> Config Class Initialized
+INFO - 2024-11-26 23:00:16 --> Hooks Class Initialized
+DEBUG - 2024-11-26 23:00:16 --> UTF-8 Support Enabled
+INFO - 2024-11-26 23:00:16 --> Utf8 Class Initialized
+INFO - 2024-11-26 23:00:16 --> URI Class Initialized
+INFO - 2024-11-26 23:00:16 --> Router Class Initialized
+INFO - 2024-11-26 23:00:16 --> Output Class Initialized
+INFO - 2024-11-26 23:00:16 --> Security Class Initialized
+DEBUG - 2024-11-26 23:00:16 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 23:00:16 --> Input Class Initialized
+INFO - 2024-11-26 23:00:16 --> Language Class Initialized
+INFO - 2024-11-26 23:00:16 --> Loader Class Initialized
+INFO - 2024-11-26 23:00:16 --> Database Driver Class Initialized
+INFO - 2024-11-26 23:00:16 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 23:00:16 --> Controller Class Initialized
+INFO - 2024-11-26 23:00:16 --> Helper loaded: url_helper
+DEBUG - 2024-11-26 23:00:16 --> Session class already loaded. Second attempt ignored.
+INFO - 2024-11-26 23:00:16 --> Model "User_model" initialized
+DEBUG - 2024-11-26 23:00:16 --> Email: clinica@gmail.com, Role: clinic
+ERROR - 2024-11-26 23:00:16 --> Severity: Warning --> SQLite3::query(): Unable to prepare statement: 1, no such table: users /Users/larger/Desktop/github/malado/convvet/convvet/system/database/drivers/sqlite3/sqlite3_driver.php 130
+ERROR - 2024-11-26 23:00:16 --> Query error: no such table: users - Invalid query: SELECT *
+FROM "users"
+WHERE "email" = 'clinica@gmail.com'
+AND "role" = 'clinic'
+INFO - 2024-11-26 23:00:16 --> Language file loaded: language/english/db_lang.php
+ERROR - 2024-11-26 23:00:16 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Users/larger/Desktop/github/malado/convvet/convvet/system/core/Exceptions.php:272) /Users/larger/Desktop/github/malado/convvet/convvet/system/core/Common.php 571
+INFO - 2024-11-26 23:00:17 --> Config Class Initialized
+INFO - 2024-11-26 23:00:17 --> Hooks Class Initialized
+DEBUG - 2024-11-26 23:00:17 --> UTF-8 Support Enabled
+INFO - 2024-11-26 23:00:17 --> Utf8 Class Initialized
+INFO - 2024-11-26 23:00:17 --> URI Class Initialized
+INFO - 2024-11-26 23:00:17 --> Router Class Initialized
+INFO - 2024-11-26 23:00:17 --> Output Class Initialized
+INFO - 2024-11-26 23:00:17 --> Security Class Initialized
+DEBUG - 2024-11-26 23:00:17 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 23:00:17 --> Input Class Initialized
+INFO - 2024-11-26 23:00:17 --> Language Class Initialized
+INFO - 2024-11-26 23:00:17 --> Loader Class Initialized
+INFO - 2024-11-26 23:00:17 --> Database Driver Class Initialized
+INFO - 2024-11-26 23:00:17 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 23:00:17 --> Controller Class Initialized
+INFO - 2024-11-26 23:00:17 --> Helper loaded: url_helper
+DEBUG - 2024-11-26 23:00:17 --> Session class already loaded. Second attempt ignored.
+INFO - 2024-11-26 23:00:17 --> Model "User_model" initialized
+INFO - 2024-11-26 23:00:17 --> File loaded: /Users/larger/Desktop/github/malado/convvet/convvet/application/views/auth/login.php
+INFO - 2024-11-26 23:00:17 --> Final output sent to browser
+DEBUG - 2024-11-26 23:00:17 --> Total execution time: 0.0423
+INFO - 2024-11-26 23:00:18 --> Config Class Initialized
+INFO - 2024-11-26 23:00:18 --> Hooks Class Initialized
+DEBUG - 2024-11-26 23:00:18 --> UTF-8 Support Enabled
+INFO - 2024-11-26 23:00:18 --> Utf8 Class Initialized
+INFO - 2024-11-26 23:00:18 --> URI Class Initialized
+INFO - 2024-11-26 23:00:18 --> Router Class Initialized
+INFO - 2024-11-26 23:00:18 --> Output Class Initialized
+INFO - 2024-11-26 23:00:18 --> Security Class Initialized
+DEBUG - 2024-11-26 23:00:18 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 23:00:18 --> Input Class Initialized
+INFO - 2024-11-26 23:00:18 --> Language Class Initialized
+INFO - 2024-11-26 23:00:18 --> Loader Class Initialized
+INFO - 2024-11-26 23:00:18 --> Database Driver Class Initialized
+INFO - 2024-11-26 23:00:18 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 23:00:18 --> Controller Class Initialized
+INFO - 2024-11-26 23:00:18 --> Helper loaded: url_helper
+DEBUG - 2024-11-26 23:00:18 --> Session class already loaded. Second attempt ignored.
+INFO - 2024-11-26 23:00:18 --> Model "User_model" initialized
+INFO - 2024-11-26 23:00:18 --> File loaded: /Users/larger/Desktop/github/malado/convvet/convvet/application/views/auth/login.php
+INFO - 2024-11-26 23:00:18 --> Final output sent to browser
+DEBUG - 2024-11-26 23:00:18 --> Total execution time: 0.1265
+INFO - 2024-11-26 23:00:22 --> Config Class Initialized
+INFO - 2024-11-26 23:00:22 --> Hooks Class Initialized
+DEBUG - 2024-11-26 23:00:22 --> UTF-8 Support Enabled
+INFO - 2024-11-26 23:00:22 --> Utf8 Class Initialized
+INFO - 2024-11-26 23:00:22 --> URI Class Initialized
+INFO - 2024-11-26 23:00:22 --> Router Class Initialized
+INFO - 2024-11-26 23:00:22 --> Output Class Initialized
+INFO - 2024-11-26 23:00:22 --> Security Class Initialized
+DEBUG - 2024-11-26 23:00:22 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-26 23:00:22 --> Input Class Initialized
+INFO - 2024-11-26 23:00:22 --> Language Class Initialized
+INFO - 2024-11-26 23:00:22 --> Loader Class Initialized
+INFO - 2024-11-26 23:00:22 --> Database Driver Class Initialized
+INFO - 2024-11-26 23:00:22 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-26 23:00:22 --> Controller Class Initialized
+INFO - 2024-11-26 23:00:22 --> Helper loaded: url_helper
+DEBUG - 2024-11-26 23:00:22 --> Session class already loaded. Second attempt ignored.
+INFO - 2024-11-26 23:00:22 --> Model "User_model" initialized
+DEBUG - 2024-11-26 23:00:22 --> Email: clinica@gmail.com, Role: clinic
+ERROR - 2024-11-26 23:00:22 --> Severity: Warning --> SQLite3::query(): Unable to prepare statement: 1, no such table: users /Users/larger/Desktop/github/malado/convvet/convvet/system/database/drivers/sqlite3/sqlite3_driver.php 130
+ERROR - 2024-11-26 23:00:22 --> Query error: no such table: users - Invalid query: SELECT *
+FROM "users"
+WHERE "email" = 'clinica@gmail.com'
+AND "role" = 'clinic'
+INFO - 2024-11-26 23:00:22 --> Language file loaded: language/english/db_lang.php
+ERROR - 2024-11-26 23:00:22 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Users/larger/Desktop/github/malado/convvet/convvet/system/core/Exceptions.php:272) /Users/larger/Desktop/github/malado/convvet/convvet/system/core/Common.php 571
